5a1280c70ae1c30a2fa84a7835cfb70b46729bc1
CarolinaFreitas <carolinafreits@gmail.com>
2020-12-16 09:08:18 -0300

    Update

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("sidenav").style.width = "250px";
+    document.getElementById("sidenav").style.display = "flex";
+    ;document.getElementById("sidenav").style.width = "100%";
+    
+    
+    document.getElementById("hamburguer").onclick = "closeNav()";
+    document.getElementById("hamburguer").style.backgroundColor = "blue";
   }
   
   /* Set the width of the side navigation to 0 */
   function closeNav() {
     document.getElementById("sidenav").style.width = "0";
+
+    document.getElementById("hamburguer").onclick = "openNav()";
+    document.getElementById("hamburguer").style.backgroundColor = "red";
   } 
