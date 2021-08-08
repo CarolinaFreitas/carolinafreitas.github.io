552e72be5744c30e8f87ad7e7116f7047205e078
CarolinaFreitas <carolinafreits@gmail.com>
2021-08-08 20:18:45 -0300

    add grained.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-/* Set the width of the side navigation to 250px */
+/*
+
+
 function openNav() {
     document.getElementById("sidenav").style.display = "flex";
     ;document.getElementById("sidenav").style.width = "100%";
@@ -8,10 +10,30 @@ function openNav() {
     document.getElementById("hamburguer").style.backgroundColor = "blue";
   }
   
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
+
+    function closeNav() {
     document.getElementById("sidenav").style.width = "0";
 
     document.getElementById("hamburguer").onclick = "openNav()";
     document.getElementById("hamburguer").style.backgroundColor = "red";
+
+
   } 
+  
+  
+  */
+
+var options = {
+  "animate": true,
+  "patternWidth": 100,
+  "patternHeight": 100,
+  "grainOpacity": 0.05,
+  "grainDensity": 1,
+  "grainWidth": 3.97,
+  "grainHeight": 1.59
+}
+grained("#elementId", options);
+
+
+
+grained(body, options)
