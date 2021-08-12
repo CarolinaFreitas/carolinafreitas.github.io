8e7c13152a51ca42108ecac7567aec2b44425da1
CarolinaFreitas <carolinafreits@gmail.com>
2021-08-12 11:24:12 -0300

    add imagens

--- script.js
+++ script.js
@@ -1,39 +1,36 @@
+var options = {
+  animate: true,
+  patternWidth: 100,
+  patternHeight: 100,
+  grainOpacity: 0.09,
+  grainDensity: 1,
+  grainWidth: 1,
+  grainHeight: 1
+};
+grained('#quadrado', options);
+grained('#container', options);
+grained('#bio', options);
 /*
 
-
 function openNav() {
-    document.getElementById("sidenav").style.display = "flex";
-    ;document.getElementById("sidenav").style.width = "100%";
-    
-    
-    document.getElementById("hamburguer").onclick = "closeNav()";
-    document.getElementById("hamburguer").style.backgroundColor = "blue";
-  }
-  
-
-    function closeNav() {
-    document.getElementById("sidenav").style.width = "0";
-
-    document.getElementById("hamburguer").onclick = "openNav()";
-    document.getElementById("hamburguer").style.backgroundColor = "red";
-
-
-  } 
+  document.getElementById("sidenav").style.display = "flex";
+  ;document.getElementById("sidenav").style.width = "100%";
   
   
-  */
-
-var options = {
-  "animate": true,
-  "patternWidth": 100,
-  "patternHeight": 100,
-  "grainOpacity": 0.05,
-  "grainDensity": 1,
-  "grainWidth": 3.97,
-  "grainHeight": 1.59
+  document.getElementById("hamburguer").addEventListener("click", closeNav);
+  document.getElementById("hamburguer").style.backgroundColor = "blue";
 }
-grained("#elementId", options);
 
 
+  function closeNav() {
+  document.getElementById("sidenav").style.width = "0";
 
-grained(body, options)
+  document.getElementById("hamburguer").addEventListener("click", openNav);
+  document.getElementById("hamburguer").style.backgroundColor = "purple";
+
+
+} 
+
+*/
+  
+
